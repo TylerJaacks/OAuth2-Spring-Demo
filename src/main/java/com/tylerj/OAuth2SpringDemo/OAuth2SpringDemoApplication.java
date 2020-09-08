96d6e151a246623dc59006d6513925dad0b972aa
Tyler Jaacks <TylerJaacks@Gmail.com>
2020-09-07 19:31:26 -0500

    Added Spring Project to the Repo.
    Adding Spring Project.

---
+++ src/main/java/com/tylerj/OAuth2SpringDemo/OAuth2SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tylerj.OAuth2SpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2SpringDemoApplication.class, args);
+	}
+
+}
